a0addbbd35b9d21a4c78e3b2a5e186f702cf42d9
jleung <jleung@carbonite>
2016-10-12 13:16:01 -0400

    refactor followed_by.js

--- followed_by.js
+++ followed_by.js
@@ -3,52 +3,58 @@ var _ = require('underscore');
 var Promise = require('bluebird');
 var Client = require('instagram-private-api').V1;
 
-fs.readFile('config.json', 'utf8', function (err,data) {
-    if (err) {
-        return console.log(err);
-    }
+Promise.promisifyAll(fs);
 
-    var json = JSON.parse(data);
-    var username = json.username;
-    var password = json.password;
-    var devicename = json.device;
-    var followersfile = json.followers;
+var config = {}
+var followers = {};
 
-    var device = new Client.Device(devicename);
-    var storage = new Client.CookieFileStorage(__dirname + '/cookies/' + devicename + '.json');
-    Client.Session.create(device, storage, username, password)
-        .then(function(session) {
-            return [session, session.getAccountId()]
-        })
-        .spread(function(session, accountId) {
-            var followers = {};
-            var count = 0;
-            return new Promise(function(resolve, reject) {
-                var feed = new Client.Feed.AccountFollowers(session, accountId);
-                var fetchFollowers = function() {
-                    return feed.get().delay(1000).then(function(results) {
-                        var accounts = _.flatten(results);
-                        _.each(accounts, function(account) {
-                            followers[account.id] = account._params.username;
-                        });
-                        count = count + 1;
-                        if (feed.isMoreAvailable() && count < 3) {
-                            fetchFollowers();
-                        }
-                        else {
-                            return resolve(followers);
-                        }
-                    });
-                };
-                fetchFollowers();
-            });
-        })
-        .then(function(followers) {
-            fs.writeFile(followersfile, JSON.stringify(followers), function(err) {
-                if(err) {
-                    return console.log(err);
+fs.readFileAsync('config.json', 'utf-8')
+.then(JSON.parse)
+.then(function (json) {
+    config = json;
+    var device = new Client.Device(config.device);
+    var storage = new Client.CookieFileStorage(__dirname + '/cookies/' + config.device + '.json');
+    return Client.Session.create(device, storage, config.username, config.password);
+})
+.then(function (session) {
+    config.session = session;
+    return session.getAccountId();
+})
+.then(function (accountId) {
+    config.accountId = accountId;
+})
+.then(function() {
+    var count = 0;
+    return new Promise(function(resolve, reject) {
+        var feed = new Client.Feed.AccountFollowers(config.session, config.accountId);
+        var fetchFollowers = function() {
+            return feed.get().delay(1000).then(function(results) {
+                var accounts = _.flatten(results);
+                _.each(accounts, function(account) {
+                    followers[account.id] = account._params.username;
+                });
+                count = count + 1;
+                if (feed.isMoreAvailable() && count < 3) {
+                    fetchFollowers();
                 }
-                console.log('Followers saved: ' + Object.keys(followers).length);
+                else {
+                    return resolve(followers);
+                }
             });
-        });
+        };
+        fetchFollowers();
+    });
+})
+.then(JSON.stringify)
+.then(function(output) {
+    fs.writeFileAsync(config.followers, output);
+})
+.then(function () {
+    console.log('Followers saved: ' + Object.keys(followers).length);
+})
+.catch(SyntaxError, function (e) {
+    console.error(e);
+})
+.catch(function (e) {
+    console.error(e);
 });
